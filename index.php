a47e3e65202dfab8d39ab4987c61eaae45d9ebf2
laurentdebize <67120902+laurentdebize@users.noreply.github.com>
2024-04-10 14:10:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice <?p hp echo 'PHP'; ?> </title>
+    <title>Document</title>
 </head>
-
 <body>
-
-    <?php
-    echo '<h1>Hello world !</h1>';
-    ?>
-
-    <p>Bonjour, il fait <?php echo 'beau'; ?></p>
-
-    <?php
-    $age = 25;
-    echo $age;
-
-    $personne = array('nom' => 'Martin', 'prenom' => 'Monique', 'age' => 50);
-    echo '<pre>';
-    var_dump($personne);
-    echo '</pre>';
-    ?>
-
+    <h1>Toto</h1>
 </body>
-
 </html>
